e0646963ae9d05965ef7aab7ab509d9d049ba7a5
RJ Nowling <rnowling@gmail.com>
2023-06-18 06:28:24 -0400

    Add talks

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ DISPLAY_PAGES_ON_MENU = False
 MENUITEMS = (
     ('Home', '/'),
     ('Course Materials', '/pages/course-materials.html'),
-    ('Articles', '/pages/articles.html'),
+    ('Articles and Talks', '/pages/articles.html'),
     ('GitHub', 'https://github.com/msoe-dise-project'),
     ('Project Members', '/pages/project-members.html'),
     )
